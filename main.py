8efc4ead5b85111fc9a4c32f879e269f0a7eb426
Guilherme Colusso <80711211+guiihcolusso@users.noreply.github.com>
2023-03-14 13:31:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import csv
+import re
 import concurrent.futures
 
-# Função que realiza a busca no arquivo csv
+# Função que realiza a busca no arquivo csv usando regex
 def search_csv(filename, search_term):
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        pattern = re.compile(search_term)
         for row in reader:
-            # Verifica se o termo de busca está presente na linha atual
-            if search_term in row:
-                # Retorna a linha em que o termo foi encontrado
+            # Verifica se o padrão de regex está presente na linha atual
+            if any(pattern.search(col) for col in row):
+                # Retorna a linha em que o padrão foi encontrado
                 return row
-        # Se o termo de busca não foi encontrado, retorna None
+        # Se o padrão de regex não foi encontrado, retorna None
         return None
 
 if __name__ == '__main__':
     filename = 'large_file.csv'
     
-    # Solicita que o usuário informe o termo de busca
-    search_term = input("Digite o termo de busca: ")
+    # Solicita que o usuário informe o padrão de regex para busca
+    search_term = input("Digite o padrão de regex para busca: ")
     
     # Executa a busca em paralelo com múltiplos processos
     with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -26,7 +28,7 @@ if __name__ == '__main__':
         
         # Itera sobre os futuros na ordem de conclusão
         for future in concurrent.futures.as_completed(futures):
-            # Se o termo for encontrado, imprime as informações da linha e interrompe a execução
+            # Se o padrão de regex for encontrado, imprime as informações da linha e interrompe a execução
             result = future.result()
             if result is not None:
                 print('Found:', result)
